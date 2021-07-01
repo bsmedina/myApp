e82bbf3fe75ddfa6b26f3261e08f28036bb49e40
Beatriz Medina <bee.a2@hotmail.com>
2021-07-01 19:56:40 -0300

    Tarefa: Criacao de route.get

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const controllerServicosComDesconto = require('../controllers/listarServicosDesconto');
+const controllerListarTodosServicos = require('../controllers/listarTodosServicos');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/servicos', function (req, res, next){
+  const {taxaDesconto} = req.query;
+
+  const listaComDesconto = controllerServicosComDesconto.retornarListaServicos(taxaDesconto);
+  const listarTodos = controllerListarTodosServicos.retornarListaTodosServicos();
+
+  res.render('index', { 
+    desconto: listaComDesconto,
+    listaSemDesconto: listarTodos
+  });
+  
 });
 
 module.exports = router;
