7d0ab15af5a204d113525fe5496ed64767d6c958
Beatriz Medina <bee.a2@hotmail.com>
2021-07-01 19:55:56 -0300

    Tarefa: Criando model para os servicos

---
+++ models/servicosModels.js
@@ -0,0 +1,6 @@
+const listaServicos = require('../arquivo/servicos.json');
+
+exports.listarTodosServicos = () => {
+    return listaServicos;
+    // exportar função de listar todos os serviços
+};
