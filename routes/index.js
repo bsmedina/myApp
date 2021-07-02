d35d1b2e8d943871abb26881017476b2ee2d1fb8
Beatriz Medina <bee.a2@hotmail.com>
2021-07-01 22:05:14 -0300

    Tarefa: ajuste na router

--- routes/index.js
+++ routes/index.js
@@ -10,16 +10,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/servicos', function (req, res, next){
-  const {taxaDesconto} = req.query;
+  const {taxaDesconto} = req.query; // Irá aparecer assim na url http://localhost:3000/servicos?taxaDesconto=10, onde o 10 é o desconto.
 
-  const listaComDesconto = controllerServicosComDesconto.retornarListaServicos(taxaDesconto);
+  const listaComDesconto = controllerServicosComDesconto.retornarListaServicos(taxaDesconto || 0);
   const listarTodos = controllerListarTodosServicos.retornarListaTodosServicos();
 
   res.render('index', { 
     desconto: listaComDesconto,
     listaSemDesconto: listarTodos
   });
-  
+
 });
 
 module.exports = router;
